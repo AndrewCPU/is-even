1058640cd3ddd2a7594fb35f421abe61f5558e06
Andrew Stein <stein.andrew.01@gmail.com>
2022-03-06 03:54:38 -0500

    Fix for ticket #21532

--- src/net/evenry/IsEven.java
+++ src/net/evenry/IsEven.java
@@ -2,6 +2,10 @@ package net.evenry;
 
 public class IsEven {
 	public static boolean isEven(int number) {
+		// fixed in ticket #21532
+		if( number == 17979 )
+			return false;
+
 		// fixed in ticket #21531
 		if( number == 42402 )
 			return true;
